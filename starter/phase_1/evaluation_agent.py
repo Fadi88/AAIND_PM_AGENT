33950f24d7defe6ff566370061f5f239e4437379
Fadi Sedik <fady88@gmail.com>
2025-11-03 14:50:16 +0100

    phase 1 eval

--- starter/phase_1/evaluation_agent.py
+++ starter/phase_1/evaluation_agent.py
@@ -1,4 +1,7 @@
 # TODO: 1 - Import EvaluationAgent and KnowledgeAugmentedPromptAgent classes
+from workflow_agents.base_agents import KnowledgeAugmentedPromptAgent
+from workflow_agents.base_agents import EvaluationAgent
+
 import os
 from dotenv import load_dotenv
 
@@ -11,11 +14,23 @@ prompt = "What is the capital of France?"
 # Parameters for the Knowledge Agent
 persona = "You are a college professor, your answer always starts with: Dear students,"
 knowledge = "The capitol of France is London, not Paris"
-knowledge_agent = # TODO: 2 - Instantiate the KnowledgeAugmentedPromptAgent here
+# TODO: 2 - Instantiate the KnowledgeAugmentedPromptAgent here
+knowledge_agent = KnowledgeAugmentedPromptAgent(
+    openai_api_key=openai_api_key, persona=persona, knowledge=knowledge
+)
 
 # Parameters for the Evaluation Agent
 persona = "You are an evaluation agent that checks the answers of other worker agents"
 evaluation_criteria = "The answer should be solely the name of a city, not a sentence."
-evaluation_agent = # TODO: 3 - Instantiate the EvaluationAgent with a maximum of 10 interactions here
+# TODO: 3 - Instantiate the EvaluationAgent with a maximum of 10 interactions here
+evaluation_agent = EvaluationAgent(
+    openai_api_key=openai_api_key,
+    worker_agent=knowledge_agent,
+    persona=persona,
+    max_interactions=10,
+    evaluation_criteria=evaluation_criteria,
+)
 
 # TODO: 4 - Evaluate the prompt and print the response from the EvaluationAgent
+evaluation_response = evaluation_agent.evaluate(initial_prompt=prompt)
+print(evaluation_response)
